6ae7afee37bd4a50c3f95268c0ededa3b59e06f0
Duy Khang <duykhang2103@gmail.com>
2024-12-02 22:27:39 +0700

    modify exit

--- Assets/Scripts/Achievements/Exit.cs
+++ Assets/Scripts/Achievements/Exit.cs
@@ -1,7 +1,9 @@
+using System;
 using UnityEngine;
 
 public class Exit : MonoBehaviour {
   public GameManager gameManager;
+  public Boolean isVisible;
 
   public void Update() {
     if (Input.GetKeyDown(KeyCode.Escape)) {
