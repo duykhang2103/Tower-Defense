946b989db86aff6f291ee5bd91daaf90cf2c7f14
Duy Khang <duykhang2103@gmail.com>
2024-12-05 22:09:17 +0700

    modify tutorial

--- Assets/Scripts/Managers/TutorialManager.cs
+++ Assets/Scripts/Managers/TutorialManager.cs
@@ -2,22 +2,37 @@ using UnityEngine;
 
 public class TutorialManager : MonoBehaviour {
   public int curStep;
-  public GameObject[] objectsToHit;
-  public string textToDisplay;
+  private string[] objectsToHit = { "TextBanner", "OpenSpot_1", "ShootingTowerBtn", "OpenSpot_2", "SummoningTowerBtn", };
+  private string[] textsToDisplay = {
+    // "Welcome to the tutorial! CLick on this banner to continue.",
+    "This is a tower spot. Click on it to build a tower.",
+    "This is the shooting tower button. Click on it to build a shooting tower.",
+    "This is another tower spot. Click on it to build a tower.",
+    "This is the summoning tower button. Click on it to build a summoning tower.",
+  };
 
-  public GameObject[] hitClicksToContinue;
+  public ClickedDetector clickedDetector;
 
   void Start() {
     curStep = 0;
   }
 
   void Update() {
-    if (Input.GetKeyDown(KeyCode.Space)) {
-      curStep++;
-    }
-  }
+    if (Input.GetMouseButtonDown(0)) {
+    Debug.Log(curStep);
+    Debug.Log(clickedDetector.GetClickedObjectName());
+      if (clickedDetector.GetClickedObjectName() == objectsToHit[curStep]) {
+        GameObject textBanner = GameObject.Find("TextBanner");
+        // Debug.Log(textBanner);
+        // Canvas canvas = textBanner.Find("Canvas");
+        GameObject textMesh = GameObject.Find("TextMesh");
+        // Debug.Log(textMesh);
+        textMesh.GetComponent<TMPro.TextMeshProUGUI>().text = textsToDisplay[curStep];
+        // canvas.GetComponent<TMPro.TextMeshProUGUI>().text = textsToDisplay[curStep];
+        // textBanner.GetComponent<TMPro.TextMeshProUGUI>().text = textsToDisplay[curStep];
+        curStep++;
+      }
 
-  public void displayText(string text) {
-    textToDisplay = text;
+    }
   }
 }
